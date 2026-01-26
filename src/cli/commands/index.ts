f230b2d3f0d04fd0761358d3d2c9479d0d152f10
takemo101 <takemo101@gmail.com>
2026-01-27 08:53:07 +0900

    Sort exports alphabetically

--- src/cli/commands/index.ts
+++ src/cli/commands/index.ts
@@ -4,24 +4,23 @@
  * v2.0.0 F-102: CLIリファクタリング
  */
 
-export { runCommand } from "./run.js";
-export { statusCommand } from "./status.js";
-export { initCommand } from "./init.js";
-export { logsCommand } from "./logs.js";
-export { eventsCommand } from "./events.js";
-export { emitCommand } from "./emit.js";
 export { cancelCommand } from "./cancel.js";
 export { clearCommand } from "./clear.js";
+export { emitCommand } from "./emit.js";
+export { eventsCommand } from "./events.js";
+export { initCommand } from "./init.js";
+export { logsCommand } from "./logs.js";
 export { replayCommand } from "./replay.js";
+export { runCommand } from "./run.js";
+export { statusCommand } from "./status.js";
 export { toolsCommand } from "./tools.js";
-
-export type { CommandHandler } from "./types.js";
 export type {
+	CancelCommandOptions,
+	CommandHandler,
+	EmitCommandOptions,
+	InitCommandOptions,
+	LogsCommandOptions,
 	RunCommandOptions,
 	StatusCommandOptions,
-	LogsCommandOptions,
-	CancelCommandOptions,
-	InitCommandOptions,
-	EmitCommandOptions,
 	TaskToolOptions,
 } from "./types.js";
